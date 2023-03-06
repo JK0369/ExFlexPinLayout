fe743825891293f97088dee9bc332a726fb87ce7
jake <palatable7@naver.com>
2023-03-07 00:36:04 +0900

    Add

--- ExFlexLayout/VC4.swift
+++ ExFlexLayout/VC4.swift
@@ -1,20 +1,21 @@
 //
-//  ViewController.swift
+//  VC4.swift
 //  ExFlexLayout
 //
-//  Created by 김종권 on 2023/03/06.
+//  Created by 김종권 on 2023/03/07.
 //
 
 import UIKit
 import FlexLayout
 import PinLayout
 
-class ViewController: UIViewController {
+// shrink, grow
+final class VC4: UIViewController {
     private let container = UIView()
     private let label1: UILabel = {
         let label = UILabel()
         label.text = "label1"
-        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.font = .systemFont(ofSize: 30, weight: .bold)
         label.numberOfLines = 0
         label.backgroundColor = .lightGray
         return label
@@ -22,7 +23,7 @@ class ViewController: UIViewController {
     private let label2: UILabel = {
         let label = UILabel()
         label.text = "label2"
-        label.font = .systemFont(ofSize: 25, weight: .bold)
+        label.font = .systemFont(ofSize: 50, weight: .bold)
         label.numberOfLines = 0
         label.backgroundColor = .systemBlue
         return label
@@ -30,6 +31,7 @@ class ViewController: UIViewController {
     private let label3: UILabel = {
         let label = UILabel()
         label.text = "label3"
+//        label.font = .systemFont(ofSize: 70, weight: .bold)
         label.font = .systemFont(ofSize: 30, weight: .bold)
         label.numberOfLines = 0
         label.backgroundColor = .blue
@@ -40,41 +42,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(container)
-        container.flex.alignItems(.center).define { flex in
-            flex.addItem(label1)
-                .marginTop(10)
-            flex.addItem(label2)
-            flex.addItem(label3)
-        }
+        modalPresentationStyle = .fullScreen
+        view.backgroundColor = .white
         
-        container.flex.justifyContent(.center).define { flex in
+        view.addSubview(container)
+//        container.flex.direction(.row).define { flex in
+//            flex.addItem(label1)
+//                .shrink(3)
+//            flex.addItem(label2)
+//                .shrink(1)
+//            flex.addItem(label3)
+//                .shrink(1)
+//        }
+        
+        container.flex.direction(.row).define { flex in
             flex.addItem(label1)
-                .marginTop(10)
             flex.addItem(label2)
             flex.addItem(label3)
+                .grow(1.5)
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
-            // tableView
-//            self.present(VC2(), animated: true)
-            
-            // scrollView
-//            self.present(VC3(), animated: true)
-            
-            // shrink, grow
-            self.present(VC4(), animated: true)
-        })
-    }
-
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        container.pin.all(view.pin.safeArea)
+        container.pin.all()
         container.flex.layout()
     }
 }
